3cc96920f6cd68b43fcf1d1b0a347c71d6f8ffdd
vern97 <49202824+vern97@users.noreply.github.com>
2019-04-04 00:23:11 -0700

    Update RectangularPrism.swift

--- RectangularPrism.swift
+++ RectangularPrism.swift
@@ -9,12 +9,36 @@ import Foundation
 
 class RectangularPrism{
     
-    private var rp_length, rp_width, rp_height: Int
+    private var length, width, height: Double
     
-    init(length: Int, width: Int, height: Int){
-        rp_length = length
-        rp_width = width
-        rp_height = height
+    init(length: Double, width: Double, height: Double){
+        self.length = length
+        self.width = width
+        self.height = height
+    }
+    
+    public func calculateSurfaceArea() -> Double{
+        var length_times_width, length_times_height, width_times_height: Double
+        length_times_width = (length * width)    //Product of length and width
+        length_times_height = (length * height)  //Product of length and height
+        width_times_height = (width * height)    //Product of width and height
+        return (2 * ((length_times_width) + (length_times_height) + (width_times_height))) //Calculate surface area
+    }
+    
+    public func calculateVolume() -> Double{
+        return ((length) * (width) * (height)) //Calculate volume
+    }
+    
+    public func getLength() -> Double{
+        return length
+    }
+    
+    public func getWidth() -> Double{
+        return width
+    }
+    
+    public func getHeight() -> Double{
+        return height
     }
     
 }
