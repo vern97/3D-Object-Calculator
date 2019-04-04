316887fc592a33595704219829b3899de1dfed71
vern97 <49202824+vern97@users.noreply.github.com>
2019-04-04 00:10:41 -0700

    Rectangular prism class file

---
+++ RectangularPrism.swift
@@ -0,0 +1,20 @@
+//
+//  RectangularPrism.swift
+//
+//  Created by Brandon Linton and Noah Castle on 4/3/19.
+//  Copyright © 2019 Brandon Linton and Noah Castle. All rights reserved.
+//
+
+import Foundation
+
+class RectangularPrism{
+    
+    private var rp_length, rp_width, rp_height: Int
+    
+    init(length: Int, width: Int, height: Int){
+        rp_length = length
+        rp_width = width
+        rp_height = height
+    }
+    
+}
